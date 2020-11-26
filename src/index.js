f005b90150d512de568d2f478b7f36bfb5948261
Sofia <sofia.dionisio94@gmail.com>
2020-11-26 16:47:43 +0000

    Created actions

--- src/index.js
+++ src/index.js
@@ -1,24 +1,8 @@
 import store from './store';
-import * as actions from './actionTypes';
+import { bugAdded, bugResolved } from './actions';
 
-const unsubscribe = store.subscribe(() => {
-	console.log('Store changed', store.getState());
-});
+store.dispatch(bugAdded('Bug 1'));
 
-store.dispatch({
-	type: actions.BUG_ADDED,
-	payload: {
-		description: 'Bug1',
-	},
-});
-
-unsubscribe();
-
-store.dispatch({
-	type: actions.BUG_REMOVED,
-	payload: {
-		id: 1,
-	},
-});
+store.dispatch(bugResolved(1));
 
 console.log(store.getState());
